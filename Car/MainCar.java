54b4e147a530923e1c4d9fae6f435dae990796a8
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-10-08 00:24:34 -0300

    ajustes de pasta

--- Car/MainCar.java
+++
@@ -1,8 +0,0 @@
-public class MainCar {
-    public static void main(String[] args) {
-        Car car = new Car("Short bonito", 4123, 49.99);
-        System.out.println(car.Honk());
-        System.out.println(car.Display());  
-        
-    }
-}
