52e4646d5dadc73b55e8bca772f9b63c5657cad6
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-10-08 00:19:55 -0300

    Create package Car

---
+++ Car/MainCar.java
@@ -0,0 +1,8 @@
+public class MainCar {
+    public static void main(String[] args) {
+        Car car = new Car("Short bonito", 4123, 49.99);
+        System.out.println(car.Honk());
+        System.out.println(car.Display());  
+        
+    }
+}
