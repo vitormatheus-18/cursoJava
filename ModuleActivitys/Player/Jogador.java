7953e4df4f5d5aa71949d5dcdd8d9218f5c65f58
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-10-15 20:24:45 -0300

    Alter package Player

--- ModuleActivitys/Player/Jogador.java
+++ ModuleActivitys/Player/Jogador.java
@@ -1,5 +1,6 @@
 
 import java.time.LocalDate;
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
@@ -8,10 +9,10 @@ public class Jogador {
     private int Id;
     private String Nome;
     private String Apelido;
-    LocalDate Date;
+    private LocalDate Date;
     private int Numero;
     private String Posicao;
-    private int Qualidade;
+    private double Qualidade;
     private int Cartoes;
     private boolean Suspenso;
     
@@ -26,30 +27,158 @@ public class Jogador {
         Id = id;
         Nome = nome;
         Apelido = apelido;
-        setDate(date);
+        Date = date;
         Numero = numero;
         Posicao = posicao;
         Qualidade = qualidade;
         Cartoes = cartoes;
         Suspenso = suspenso;
+        
+    }
+    
+    public boolean verificaCondicaoJogo(){
 
+        if(Cartoes >= 3){
+            Suspenso = true;
+            return false;
+        }else 
+           return true;
+    }
+
+    public void aplicarCartao(int quantidade){
+        Cartoes += quantidade;
+
+        if(Cartoes >= 3){
+            Suspenso = true; 
+        }
+       
+    }
+
+    public void cumprirSuspensao(){
+        Cartoes = 0;
+        Suspenso = false;
+    }
+
+    public void sofrerLesao(){
+        double novaQualidade = Qualidade;
+
+        Random random = new Random();
+        int probabilidade = random.nextInt(101);
+        if(probabilidade < 5) {            
+            novaQualidade = Qualidade - (15 * Qualidade / 100);
+        } else if (probabilidade <= 15) {            
+            novaQualidade = Qualidade - (10 * Qualidade / 100);
+        } else if (probabilidade <= 30) {            
+            novaQualidade = Qualidade - (5 * Qualidade / 100);
+        } else if (probabilidade <= 60) {            
+            novaQualidade = Qualidade - 2;
+        } else if (probabilidade <= 100) {            
+            novaQualidade = Qualidade - 1;
+        }
+
+        if (novaQualidade >= 0) {
+            Qualidade = novaQualidade;
+        } else {
+            Qualidade = 0;
+        }
+    }
+
+    public void Training() {
+        double novaQualidade = Qualidade;
+
+        Random random = new Random();
+        int probabilidade = random.nextInt(101);
+        if(probabilidade < 5) {
+            novaQualidade = Qualidade + 5;
+        } else if (probabilidade <= 15) {
+            novaQualidade = Qualidade + 4;
+        } else if (probabilidade <= 30) {
+            novaQualidade = Qualidade + 3;
+        } else if (probabilidade <= 60) {
+            novaQualidade = Qualidade + 2;
+        } else if (probabilidade <= 100) {
+            novaQualidade = Qualidade + 1;
+        }
+
+        if (novaQualidade <= 100) {
+            Qualidade = novaQualidade;            
+        } else {
+            Qualidade = 100;
+        }
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getApelido() {
+        return Apelido;
+    }
+
+    public void setApelido(String apelido) {
+        Apelido = apelido;
+    }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
+    public String getPosicao() {
+        return Posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        Posicao = posicao;
+    }
+
+    public double getQualidade() {
+        return Qualidade;
+    }
+
+    public void setQualidade(int qualidade) {
+        Qualidade = qualidade;
+    }
+
+    public int getCartoes() {
+        return Cartoes;
+    }
+
+    public void setCartoes(int cartoes) {
+        Cartoes = cartoes;
+    }
+
+    public boolean isSuspenso() {
+        return Suspenso;
+    }
+
+    public void setSuspenso(boolean suspenso) {
+        Suspenso = suspenso;
     }
 
     private void setDate(LocalDate date) {
         this.Date = date;
     }
 
-    public void verficaCondicaoJogo(int cartoes, boolean suspenso){
-
-        Cartoes = cartoes;
-        Suspenso = suspenso;
-
-        if(Cartoes >= 3){
-            Suspenso = true;
-            System.out.println(Numero + " - " + Nome + " - " + LocalDate.now() + ", Condição: este Jogador está suspenso");
-        }else 
-            System.out.println(Numero + " - " + Nome + " - " + LocalDate.now() + ", Condição: este Jogador está disponível");
-    }
 
 
 
--- ModuleActivitys/Player/JogadorMain.java
+++ ModuleActivitys/Player/JogadorMain.java
@@ -1,50 +1,27 @@
 import java.time.LocalDate;
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class JogadorMain{
 
-    // public static void main(String[] args) {
-    //     ArrayList<Jogador> jogador = new ArrayList<>();
-        
-    //     Jogador jogadores = new Jogador(1, "Mailson", "paredão", LocalDate.now(), 1, "Goleiro", 1, 0, false );
-    //     jogador.add(jogadores);
-    //     jogador.add(new Jogador(2,"Ewerthon", "N", LocalDate.now(), 66, "LateralD", 1, 0, false ));
-    //     jogador.add(new Jogador(3, "Chico", "N", LocalDate.now(), 4, "Zagueiro", 1, 2, false ));
-    //     jogador.add(new Jogador(4, "Sabino", "xerife", LocalDate.now(), 35, "Zagueiro", 1, 0, false ));
-    //     jogador.add(new Jogador(5, "Sander", "N", LocalDate.now(), 6, "lateralE", 1, 0, false ));
-    //     jogador.add(new Jogador(6, "Marcão", "N", LocalDate.now(), 77, "Volante", 1, 3, false));
-    //     jogador.add(new Jogador(7, "Hernanes", "profeta", LocalDate.now(), 8, "Volante", 1, 0, false));
-    //     jogador.add(new Jogador(8, "José Welison", "N", LocalDate.now(), 14, "Volante", 1, 1, false ));
-    //     jogador.add(new Jogador(9, "Gustavo", "aspira", LocalDate.now(), 10, "Meio-campo", 1, 0, false));
-    //     jogador.add(new Jogador(10, "Mikael", "goleador", LocalDate.now(), 99, "Atacante", 1, 0, false));
-    //     jogador.add(new Jogador(11, "Paulinho Moccelin", "N", LocalDate.now(), 94, "Atacante", 1, 0, false ));
-
-
-    
     public static void main(String[] args) {
-        Jogador j1 = new Jogador(1, "Mailson", "paredão", LocalDate.now(), 1, "Goleiro", 1, 0, false );
-        j1.verficaCondicaoJogo(0, false);
-        Jogador j2 = new Jogador(2, "Ewerthon", "N", LocalDate.now(), 66, "LateralD", 1, 0, false );
-        j2.verficaCondicaoJogo(0, false);
-        Jogador j3 = new Jogador(3, "Chico", "N", LocalDate.now(), 4, "Zagueiro", 1, 2, false );
-        j3.verficaCondicaoJogo(2, false);
-        Jogador j4 = new Jogador(4, "Sabino", "xerife", LocalDate.now(), 35, "Zagueiro", 1, 0, false );
-        j4.verficaCondicaoJogo(0, false);
-        Jogador j5 = new Jogador(5, "Sander", "N", LocalDate.now(), 6, "lateralE", 1, 0, false );
-        j5.verficaCondicaoJogo(0, false);
-        Jogador j6 = new Jogador(6, "Marcão", "N", LocalDate.now(), 77, "Volante", 1, 3, false );
-        j6.verficaCondicaoJogo(3, false);
-        Jogador j7 = new Jogador(7, "Hernanes", "profeta", LocalDate.now(), 8, "Volante", 1, 0, false );
-        j7.verficaCondicaoJogo(0, false);
-        Jogador j8 = new Jogador(8, "José Welison", "N", LocalDate.now(), 14, "Volante", 1, 1, false );
-        j8.verficaCondicaoJogo(1, false);
-        Jogador j9 = new Jogador(9, "Gustavo", "aspira", LocalDate.now(), 10, "Meio-campo", 1, 0, false );
-        j9.verficaCondicaoJogo(0, false);
-        Jogador j10 = new Jogador(10, "Mikael", "goleador", LocalDate.now(), 99, "Atacante", 1, 0, false );
-        j10.verficaCondicaoJogo(0, false);
-        Jogador j11 = new Jogador(11, "Paulinho Moccelin", "N", LocalDate.now(), 94, "Atacante", 1, 0, false );
-        j11.verficaCondicaoJogo(0, false);
-        
+        ArrayList<Jogador> jogadores = new ArrayList<>();
+
+        jogadores.add(new Jogador(1, "Mailson", "paredão", LocalDate.now(), 1, "Goleiro", 1, 0, false ));
+        jogadores.add(new Jogador(2,"Ewerthon", "N", LocalDate.now(), 66, "LateralD", 1, 0, false ));
+        jogadores.add(new Jogador(3, "Chico", "N", LocalDate.now(), 4, "Zagueiro", 1, 2, false ));
+        jogadores.add(new Jogador(4, "Sabino", "xerife", LocalDate.now(), 35, "Zagueiro", 1, 0, false ));
+        jogadores.add(new Jogador(5, "Sander", "N", LocalDate.now(), 6, "lateralE", 1, 0, false ));
+        jogadores.add(new Jogador(6, "Marcão", "N", LocalDate.now(), 77, "Volante", 1, 3, false));
+        jogadores.add(new Jogador(7, "Hernanes", "profeta", LocalDate.now(), 8, "Volante", 1, 0, false));
+        jogadores.add(new Jogador(8, "José Welison", "N", LocalDate.now(), 14, "Volante", 1, 1, false ));
+        jogadores.add(new Jogador(9, "Gustavo", "aspira", LocalDate.now(), 10, "Meio-campo", 1, 0, false));
+        jogadores.add(new Jogador(10, "Mikael", "goleador", LocalDate.now(), 99, "Atacante", 1, 0, false));
+        jogadores.add(new Jogador(11, "Paulinho Moccelin", "N", LocalDate.now(), 94, "Atacante", 1, 0, false ));
+
+        for(Jogador jogador: jogadores){
+            System.out.println(jogador.getNumero() + " - " + jogador.getNome() + " - " + jogador.getDate() + ", Condição: " + jogador.verificaCondicaoJogo());
+        }
     }
+
 
 }
